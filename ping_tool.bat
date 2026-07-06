8e6afe3a4338051f91066bbb28b1f18ae91a5de8
meowrypto <49491180+meowrypto@users.noreply.github.com>
2026-07-06 11:23:17 +0330

    Create ping_tool.bat

---
+++ ping_tool.bat
@@ -0,0 +1,14 @@
+@echo off
+title Google DNS Ping Tool
+echo =========================================
+echo       Starting Ping Test to 8.8.8.8      
+echo =========================================
+echo.
+
+ping 8.8.8.8
+
+echo.
+echo =========================================
+echo       Ping Test Completed.
+echo =========================================
+pause
